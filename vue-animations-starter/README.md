df5f5706e84b7b4fb2a26345d9060a3828e290c2
Widdy LOUIS <louisws@outlook.fr>
2022-02-16 23:37:40 +0100

    first commit

---
+++ vue-animations-starter/README.md
@@ -0,0 +1,13 @@
+# Vue 3 Animations
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+> ajouter des animations subtiles, des transitions de page et des transitions d'éléments
---
+++ vue-animations-starter/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <Toast v-if="showToast" />
+    <Todos @badValue="triggerToast" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast }
+  }
+}
+</script>
+
+<style>
+
+</style>
