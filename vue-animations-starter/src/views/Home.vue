e10dbca16af14d566674338ff4ed89e0259a6683
Widdy LOUIS <louisws@outlook.fr>
2022-02-20 13:37:20 +0100

    Adding CSS Keyframes

--- vue-animations-starter/src/views/Home.vue
+++ vue-animations-starter/src/views/Home.vue
@@ -28,8 +28,23 @@ export default {
 </script>
 
 <style>
- /* enter or leave classes */
+ /* enter or leave classes 
  .toast-enter-from, .toast-leave-to {opacity: 0; transform: translateY(-60px);}
  .toast-enter-to, .toast-leave-from {opacity: 1;transform: translateY(0);}
- .toast-enter-active, .toast-leave-active { transition: all .3s ease;}
+ .toast-enter-active, .toast-leave-active { transition: all .3s ease;} */
+
+ .toast-enter-active { animation: wobble .5s ease;}
+ .toast-leave-from {opacity: 1;transform: translateY(0);}
+ .toast-leave-to {opacity: 0; transform: translateY(-60px);}
+ .toast-leave-active {transition: all .3s ease;}
+
+ @keyframes wobble {
+   0% {opacity: 0; transform: translateY(-60px);}
+   50% {opacity: 1;transform: translateY(0);}
+   60%{transform: translateX(8px);}
+   70%{transform: translateX(-8px);}
+   80%{transform: translateX(4px);}
+   90%{transform: translateX(-4px);}
+   100%{transform: translateX(0);}
+ }
 </style>
