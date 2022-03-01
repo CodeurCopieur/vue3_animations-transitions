6c69384c907abd06df83fc2d4e35804cec936eac
Widdy LOUIS <louisws@outlook.fr>
2022-03-01 22:03:29 +0100

    Using GSAP

--- vue-animations-starter/src/views/About.vue
+++ vue-animations-starter/src/views/About.vue
@@ -11,7 +11,15 @@
       @after-leave="afterLeave">
       <h1 v-if="showTitle">About</h1>
     </transition>
-    
+
+    <transition
+      appear
+      @before-enter="beforeE"
+      @enter="Enter"
+      @after-enter="afterE"
+      >
+      <h1>Title Gsap</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -21,11 +29,37 @@
 <script>
 
 import {ref} from 'vue'
+import gsap from 'gsap'
 
 export default {
   setup() {
 
     const showTitle = ref(true);
+
+    const beforeE = el => {
+      console.log('before enter - définir l\'état initial');
+      gsap.from(el , {
+        y: '-60',
+        opacity: 0
+      });
+    }
+
+    const done = () => console.log('end');
+
+    const Enter = (el, done) => {
+      console.log('starting - faire la transition');
+      gsap.to(el , {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease:"bounce.out",
+        onComplete: done
+      });
+    }
+
+    const afterE = el => {
+      console.log('after enter');
+    }
 
     const beforeEnter = (el) => console.log('before Enter', el);
     const enter = (el) => console.log('enter', el);
@@ -42,7 +76,7 @@ export default {
     const leave = (el) => console.log('leave', el);
     const afterLeave = (el) => console.log('after Leave', el);
 
-    return { showTitle, beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave}
+    return { showTitle, beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave, beforeE, Enter, afterE}
   }
 }
 </script>
