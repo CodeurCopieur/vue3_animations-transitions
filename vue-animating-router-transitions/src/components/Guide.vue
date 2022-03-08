444fdc852cac3c3ff95d7bd11a6230ca99ab7f47
Widdy LOUIS <louisws@outlook.fr>
2022-03-08 21:35:32 +0100

    components

---
+++ vue-animating-router-transitions/src/components/Guide.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page mt-6 text-left">
+    <h1 class="mb-3 font-bold">Guide</h1>
+    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, necessitatibus?</p>
+    <h2 class="mb-3 font-bold">Lorem ipsum.</h2>
+    <p class="mb-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem labore inventore sint natus dolores doloremque rerum. Unde facere saepe, quo repellendus, alias, minima rem accusantium quas quaerat ut distinctio numquam.
+    </p>
+  </div>
+</template>
