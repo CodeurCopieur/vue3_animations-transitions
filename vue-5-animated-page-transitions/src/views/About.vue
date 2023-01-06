c939e607ce0d44895d0157bc375c60d2b2bd97c2
Widdy LOUIS <louisws@outlook.fr>
2023-01-06 23:55:42 +0100

    Update About.vue

--- vue-5-animated-page-transitions/src/views/About.vue
+++ vue-5-animated-page-transitions/src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
-  <main>
-    <h1>About</h1>
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <main> 
 
+    <img src="https://picsum.photos/800/300" alt="Placeholder" />
+    <h1>About</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, nobis!</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, commodi reprehenderit. Soluta maiores officia cupiditate doloremque mollitia voluptate! Rem, fugiat.</p>
   </main>
