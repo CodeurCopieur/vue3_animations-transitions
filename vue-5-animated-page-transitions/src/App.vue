c24c5ee536e802493e791b74180294a625626fc5
Widdy LOUIS <louisws@outlook.fr>
2023-01-07 00:22:29 +0100

    Update App.vue

--- vue-5-animated-page-transitions/src/App.vue
+++ vue-5-animated-page-transitions/src/App.vue
@@ -6,7 +6,7 @@
     <nav>
      <router-link to="/">Home</router-link>
      <router-link to="/about">About</router-link>
-   </nav>
+    </nav>
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component"></component>
@@ -16,16 +16,5 @@
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
 
