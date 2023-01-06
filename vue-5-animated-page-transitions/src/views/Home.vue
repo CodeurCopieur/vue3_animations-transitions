6b8e1c6667083fe4fc02fc80839cab52e3ac8fbf
Widdy LOUIS <louisws@outlook.fr>
2023-01-06 23:55:47 +0100

    Update Home.vue

--- vue-5-animated-page-transitions/src/views/Home.vue
+++ vue-5-animated-page-transitions/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
     <h1>Home</h1>
-          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, nobis!</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, commodi reprehenderit. Soluta maiores officia cupiditate doloremque mollitia voluptate! Rem, fugiat.</p>
   </main>
