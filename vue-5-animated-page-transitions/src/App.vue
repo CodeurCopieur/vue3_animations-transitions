4f37de03476b7e03bc62f485ca0071472aa2b43e
Widdy LOUIS <louisws@outlook.fr>
2023-01-07 01:09:50 +0100

    5 radio type inputs w variable anim

--- vue-5-animated-page-transitions/src/App.vue
+++ vue-5-animated-page-transitions/src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue'
+
+const anim = ref('fade');
 </script>
 
 <template>
@@ -7,8 +10,41 @@
      <router-link to="/">Home</router-link>
      <router-link to="/about">About</router-link>
     </nav>
+
+    <div class="check">
+      <label>
+        <input type="radio" 
+          name="anim" 
+          value="fade"
+          v-model="anim" /> Fade
+      </label>
+      <label>
+        <input type="radio" 
+          name="anim" 
+          value="slide"
+          v-model="anim" /> Slide
+      </label>
+      <label>
+        <input type="radio" 
+          name="anim" 
+          value="slide-down"
+          v-model="anim" /> Slide-down
+      </label>
+      <label>
+        <input type="radio" 
+          name="anim" 
+          value="grow-in"
+          v-model="anim" /> Grow-in
+      </label>
+      <label>
+        <input type="radio" 
+          name="anim" 
+          value="grow-out"
+          v-model="anim" /> Grow-out
+      </label>
+    </div>
     <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
+      <transition :name="anim" mode="out-in">
         <component :is="Component"></component>
       </transition>
     </router-view>
