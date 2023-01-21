0c3570cb03442776102d8174b4f2ef3aad91e8ab
Widdy LOUIS <louisws@outlook.fr>
2023-01-21 23:56:00 +0100

    Basic animation w/  TweenMax, Power3

--- vue-using-scrollTrigger/src/App.vue
+++ vue-using-scrollTrigger/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {ref, onMounted} from 'vue'
-import gsap from 'gsap'
+import { TweenMax, Power3, gsap} from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger);
@@ -10,31 +10,47 @@ gsap.registerPlugin(ScrollTrigger);
 import HelloWorld from './components/HelloWorld.vue'
 
 const imgRef = ref(null)
+const title1 = ref(null)
+const txt1 = ref(null)
 
-   onMounted(()=> {
-    const el = imgRef.value;
-      gsap.fromTo(el, {rotate:0}, 
-      {rotate: 360, duration: 3,
-        scrollTrigger: {
-          trigger: el,
-          start: 'center bottom-=100',
-          end: 'bottom center',
-          scrub: true,
-          markers: true
-        }
-      })
-    });
+  onMounted(()=> {
+  const el = imgRef.value;
+  const t1 = title1.value;
+  const txt = txt1.value;
+  
+  TweenMax.fromTo(t1, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut})
+  TweenMax.fromTo(txt, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut, delay: .2})
+
+  gsap.fromTo(el, 
+    {rotate:0}, 
+    {rotate: 360, duration: 3,
+      scrollTrigger: {
+        trigger: el,
+        start: 'center bottom-=100',
+        end: 'bottom center',
+        scrub: true,
+        markers: true
+      }
+    }
+  )
+  });
 
     
 </script>
 
 <template>
-  <div class="helper"></div>
+  <div class="helper">
+    <h1 ref="title1">Text2</h1>
+    <div>
+      <p ref="txt1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veniam perferendis maxime.<br/>Minus quae amet officia, quam nisi enim veritatis. <br/>
+      <a class="btn" href="#">button</a></p>
+    </div>
+  </div>
   <div style="margin-top: 100px">
     <img alt="Vue logo" src="./assets/logo.png" ref="imgRef" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
   </div>
-  <div class="helper"></div>
+  <div class="helper" style="min-height: 80vh"></div>
 </template>
 
 <style>
@@ -54,7 +70,32 @@ body {
 }
 
 .helper {
-  height: 80vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
   background: skyblue;
 }
+
+.helper h1 {
+  margin-bottom: 1em;
+}
+
+
+.helper p {
+  margin-top: 3em;
+}
+
+.btn {
+  padding: 10px 35px;
+  border: 2px solid #fff;
+  margin-top: 1em;
+  display: inline-block;
+  text-decoration: none;
+  color: #fff;
+  text-transform: capitalize;
+}
+
 </style>
