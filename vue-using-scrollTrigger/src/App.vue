e65a8dba20f953668de475fbd8b201f63ba0c68b
Widdy LOUIS <louisws@outlook.fr>
2023-01-22 01:41:07 +0100

    part 2

--- vue-using-scrollTrigger/src/App.vue
+++ vue-using-scrollTrigger/src/App.vue
@@ -9,30 +9,61 @@ gsap.registerPlugin(ScrollTrigger);
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 
-const imgRef = ref(null)
-const title1 = ref(null)
-const txt1 = ref(null)
+  const imgRef = ref(null)
+  const title1 = ref(null)
+  const txt1 = ref(null)
+
+  let parentCircle = ref(null)
+  let circleBlue = ref(null)
+  let circleWhite = ref(null)
+  let circleRed = ref(null)
+
+  let state = ref(null)
+
+  const handleExpand = (target) => {
+    TweenMax.to(target, .8, {width: 200, height:200, ease: Power3.easeOut})
+    state.value = true
+  }
+
+  const handleShrink = (target) => {
+    TweenMax.to(target, .8, {width: 75, height: 75, ease: Power3.easeOut})
+    state.value = false
+  }
 
   onMounted(()=> {
-  const el = imgRef.value;
-  const t1 = title1.value;
-  const txt = txt1.value;
-  
-  TweenMax.fromTo(t1, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut})
-  TweenMax.fromTo(txt, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut, delay: .2})
+    const el = imgRef.value;
+    const t1 = title1.value;
+    const txt = txt1.value;
 
-  gsap.fromTo(el, 
-    {rotate:0}, 
-    {rotate: 360, duration: 3,
-      scrollTrigger: {
-        trigger: el,
-        start: 'center bottom-=100',
-        end: 'bottom center',
-        scrub: true,
-        markers: true
+    const pc = parentCircle.value;
+    const cb = circleBlue.value;
+    const cw = circleWhite.value;
+    const cr = circleRed.value;
+
+    TweenMax.fromTo(t1, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut})
+    TweenMax.fromTo(txt, .8, { opacity: 0 }, {opacity: 1, y: -20, ease: Power3.easeOut, delay: .2})
+
+    gsap.fromTo(el, 
+      {rotate:0}, 
+      {rotate: 360, duration: 3,
+        scrollTrigger: {
+          trigger: el,
+          start: 'center bottom-=100',
+          end: 'bottom center',
+          scrub: true,
+          markers: true
+        }
       }
-    }
-  )
+    )
+
+    TweenMax.to(pc, 0, {visibility: 'visible'})
+    // TweenMax.from(cb, .8, {opacity: 0, x: 40, ease: Power3.easeOut})
+    // TweenMax.from(cw, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .2})
+    // TweenMax.from(cr, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .4})
+    // ou
+    TweenMax.staggerFrom([cb, cw, cr], .8, {opacity: 0, x: 40, ease: Power3.easeOut}, .2);
+
+
   });
 
     
@@ -50,7 +81,11 @@ const txt1 = ref(null)
     <img alt="Vue logo" src="./assets/logo.png" ref="imgRef" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
   </div>
-  <div class="helper" style="min-height: 80vh"></div>
+  <div class="helper" ref="parentCircle">
+    <div class="circle blue" ref="circleBlue"></div>
+    <div class="circle white" ref="circleWhite" @click="state !== true ? handleExpand(circleWhite) : handleShrink(circleWhite)"></div>
+    <div class="circle" ref="circleRed"></div>
+  </div>
 </template>
 
 <style>
@@ -83,7 +118,6 @@ body {
   margin-bottom: 1em;
 }
 
-
 .helper p {
   margin-top: 3em;
 }
@@ -96,6 +130,23 @@ body {
   text-decoration: none;
   color: #fff;
   text-transform: capitalize;
+}
+
+.circle {
+  width: 75px;
+  height: 75px;
+  border-radius: 100%;
+  background-color: red;
+  margin: 50px;
+  cursor: pointer;
+}
+
+.circle.blue {
+  background-color: blue;
+}
+
+.circle.white {
+  background-color: white;
 }
 
 </style>
