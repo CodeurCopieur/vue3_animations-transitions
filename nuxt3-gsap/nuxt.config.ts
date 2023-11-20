d8853018050697290a07da2e7191e661deaa28fe
Widdy LOUIS <louisws@outlook.fr>
2023-11-20 21:31:14 +0100

    Update nuxt.config.ts

--- nuxt3-gsap/nuxt.config.ts
+++ nuxt3-gsap/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [    '@nuxtjs/tailwindcss'  ],
+  css: [
+    '~/assets/styles.css',
+  ],
   devtools: { enabled: true }
 })
