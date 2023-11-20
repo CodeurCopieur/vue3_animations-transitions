955223a19c381b5a5a1715123e0de271bb1d94b0
Widdy LOUIS <louisws@outlook.fr>
2023-11-20 21:54:41 +0100

    Update exo1.vue

--- nuxt3-gsap/pages/exo1.vue
+++ nuxt3-gsap/pages/exo1.vue
@@ -1,5 +1,72 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// Enregistrez le plugin ScrollTrigger si nécessaire
+gsap.registerPlugin(ScrollTrigger);
+
+
+  onMounted(()=> {
+    gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.box-c', {
+  y: 400,
+  rotate: 300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: '.box-c',
+    toggleAction: "restart pause reverse pause",
+    //    Trigger Scroller
+    start: 'center center',
+    end: '+=300',
+    markers: true,// ou 1
+    scrub: 1,
+  }
+})
+
+gsap.to('.box-a', {
+  y: 400,
+  rotate: 300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: '.box-d',
+    toggleAction: "restart pause reverse pause",
+    //    Trigger Scroller
+    start: 'center center',
+    end: '+=300',
+    // markers: true,// ou 1
+    scrub: true,
+    pin: ".box-b"
+  }
+})
+  });
+  </script>
 <template>
-  <div class="bg-blue-500 text-white p-4">
-   Page Exo 1
- </div>
+     <h1 ref="title" class="bg-blue-500 text-white p-4">
+    Exo 1
+   </h1>
+   <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <header class="header">
+        <h1 class="title">ScrollTrigger</h1>
+        <!-- <button class="button">Jump to F</button> -->
+      </header>
+      <div class="boxes">
+        <!-- data-speed -->
+        <div class="box box-a" data-lag="2">a</div>
+        <div class="box box-b" data-lag="1.5">b</div>
+        <div class="box box-c" data-lag=".5">c</div>
+      </div>
+
+      <div class="box box-d">d</div>
+      <div class="box box-e">e</div>
+      <div class="box box-f">f</div>
+      <div class="line"></div>
+
+       
+    </div>
+  </div>
 </template>
