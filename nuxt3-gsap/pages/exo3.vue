319ebdd611c9b7c6abb4b66294ed35b1f843f69b
Widdy LOUIS <louisws@outlook.fr>
2023-11-22 19:39:55 +0100

    Update exo3.vue

--- nuxt3-gsap/pages/exo3.vue
+++ nuxt3-gsap/pages/exo3.vue
@@ -1,5 +1,67 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// Enregistrez le plugin ScrollTrigger si nécessaire
+gsap.registerPlugin(ScrollTrigger);
+
+let title = ref(null);
+
+  onMounted(()=> {
+    title = title.value
+    gsap.to(title, { backgroundColor: 'red'})
+
+    gsap.defaults({ease: "none", duration: 2});
+
+
+    gsap.utils.toArray('.app').forEach((element, i) => {
+      ScrollTrigger.create({
+        trigger: element,
+        start: 'top top',
+        pin: true,
+        pinSpacing: false
+      })
+    });
+  });
+  </script>
 <template>
-  <div class="bg-blue-500 text-white p-4">
-   Page Exo 3
- </div>
+  <h1 ref="title" class="bg-blue-500 text-white p-4">
+    Exo 3
+   </h1>
+ <div class="container">
+
+<div class="skyblue app">
+  <h1>Text1</h1>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veniam perferendis maxime.<br/>Minus quae amet officia, quam nisi enim veritatis. <br/><a class="btn" href="#">button</a></p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius sapiente dolor repellendus quos mollitia vitae adipisci, rem excepturi. Accusamus odio delectus laborum 
+      reprehenderit optio consectetur itaque pariatur, perspiciatis necessitatibus!
+    </p>
+  </div>
+</div>
+<div class="green app">
+  <h1>Text2</h1>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veniam perferendis maxime.<br/>Minus quae amet officia, quam nisi enim veritatis. <br/><a class="btn" href="#">button</a></p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius sapiente dolor repellendus quos mollitia vitae adipisci, rem excepturi. Accusamus odio delectus laborum 
+      reprehenderit optio consectetur itaque pariatur, perspiciatis necessitatibus!
+    </p>
+  </div>
+</div>
+<div class="orange app">
+  <h1>Text3</h1>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veniam perferendis maxime.<br/>Minus quae amet officia, quam nisi enim veritatis. <br/><a class="btn" href="#">button</a></p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius sapiente dolor repellendus quos mollitia vitae adipisci, rem excepturi. Accusamus odio delectus laborum 
+      reprehenderit optio consectetur itaque pariatur, perspiciatis necessitatibus!
+    </p>
+  </div>
+</div>
+</div>
 </template>
